501c38af07d178789a7cfe3a632bcc179b2b2160
Sajal Pakira <uniquesajal6@gmail.com>
2025-06-26 21:17:28 +0530

    modification

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from './ui/button'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+      <nav className='py-4 flex justify-between items-center'>
+        <Link>
+        <img className='h-20' src="logo.png" alt="logo image" />
+        </Link>
+        <Button variant="outline" >Login</Button>
+      </nav>
+      </>
   )
 }
 
