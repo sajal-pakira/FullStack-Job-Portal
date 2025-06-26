87533b8695da8faa2476414f983c9db91a5f6d85
Sajal Pakira <uniquesajal6@gmail.com>
2025-06-27 00:33:44 +0530

    modifications

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
-  )
-}
+    <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20">
+      <section className="text-center">
+        <h1 className="flex flex-col items-center justify-center gradient-title text-4xl font-extrabold">
+          Find your dream job{" "}
+          <span>
+            And Get{" "}
+            <img
+              src="/logo.png"
+              alt="Hirred logo"
+              className="h-14 sm:24 lg:32"
+            />
+          </span>
+        </h1>
+      </section>
+      <div></div>
+      <section></section>
+    </main>
+  );
+};
 
-export default LandingPage
+export default LandingPage;
