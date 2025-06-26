788322d466763ab550ed8e2cc9f9eb11660f8285
Sajal Pakira <uniquesajal6@gmail.com>
2025-06-26 20:46:23 +0530

    modification

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,3 +1,4 @@
+import Header from "@/components/Header";
 import React from "react";
 import { Outlet } from "react-router-dom";
 
@@ -5,10 +6,13 @@ const AppLayout = () => {
   return (
     <div>
       <div className="grid-background"></div>
-      <main className="min-h-screen">
-
-      <Outlet />
+      <main className="min-h-screen container p-10">
+        <Header />
+        <Outlet />
       </main>
+      <div className="p-10 mt-10 text-center bg-gray-800">
+        made with 💕 by SAJAL PAKIRA
+      </div>
     </div>
   );
 };
