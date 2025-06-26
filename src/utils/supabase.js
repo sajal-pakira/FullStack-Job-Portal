73f59ec768c2d6575d5f5ddf2aa9148785faf754
Sajal Pakira <uniquesajal6@gmail.com>
2025-06-27 00:07:57 +0530

    modification

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,20 +1,17 @@
-
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-
 const supabaseClient = async (supabaseAccessToken) => {
-    const supabase = createClient(supabaseUrl, supabaseKey,{
-        global:{
-            headers:{
-                Authorization:`Bearer ${supabaseAccessToken}`
-            }
-        }
-    });
-    return supabase
-}
+  const supabase = createClient(supabaseUrl, supabaseKey, {
+    global: {
+      headers: {
+        Authorization: `Bearer ${supabaseAccessToken}`,
+      },
+    },
+  });
+  return supabase;
+};
 
-export default supabaseClient
-        
+export default supabaseClient;
