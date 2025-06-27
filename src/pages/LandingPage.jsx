ffab7d2929d292d5b3f7562ff3d835b4867ee341
Sajal Pakira <uniquesajal6@gmail.com>
2025-06-27 10:21:06 +0530

    modification

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,22 +1,34 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
   return (
     <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20">
       <section className="text-center">
-        <h1 className="flex flex-col items-center justify-center gradient-title text-4xl font-extrabold sm:text-6xl lg:text-8xl">
+        <h1 className="flex flex-col items-center justify-center gradient-title text-4xl font-extrabold sm:text-6xl lg:text-8xl tracking-tighter py-4">
           Find your dream job{" "}
-          <span>
+          <span className="flex items-center gap-2 sm:gap-6">
             And Get{" "}
             <img
               src="/logo.png"
               alt="Hirred logo"
-              className="h-14 sm:24 lg:32"
+              className="h-14 sm:h-24 lg:h-32"
             />
           </span>
         </h1>
+        <p className="text-gray-300 sm:mt-4 text-xs sm:text-xl">
+          Explore thousands of job listing or Find the perfect candidate
+        </p>
       </section>
-      <div></div>
+      <div>
+        <Link to="/jobs">
+        <Button>Find Jobs</Button>
+        </Link>
+        <Link to="/post-job">
+        <Button>Post a Job</Button>
+        </Link>
+      </div>
       <section></section>
     </main>
   );
