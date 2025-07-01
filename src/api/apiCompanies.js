4fa34a52bc151485d44b7bb9df8bc4c89611e94d
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-01 09:50:44 +0530

    apiCompanies.js file added

---
+++ src/api/apiCompanies.js
@@ -0,0 +1,11 @@
+import supabaseClient from "@/utils/supabase";
+
+export async function getCompanies(token) {
+  const supabase = supabaseClient(token);
+  const { data, error } = (await supabase).from("companies").select("*");
+  if (error) {
+    console.log("Error in fetching the companies", error);
+    return null;
+  }
+  return data;
+}
