1fac879441312ade63ba1e4bc4671dd71ba9e152
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-01 13:32:38 +0530

    modification

--- src/pages/Job.jsx
+++ src/pages/Job.jsx
@@ -1,7 +1,9 @@
 import { getSingleJob } from "@/api/apiJobs";
 import useFetch from "@/hooks/useFetch";
 import { useUser } from "@clerk/clerk-react";
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { BarLoader } from "react-spinners";
 
 const Job = () => {
   const { isLoaded, user } = useUser();
@@ -14,6 +16,15 @@ const Job = () => {
   } = useFetch(getSingleJob, {
     job_id: id,
   });
+
+  useEffect(() => {
+    if (isLoaded) fnJob();
+  }, [isLoaded]);
+
+  if (!isLoaded || loadingJob) {
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+  }
+
   return <div></div>;
 };
 
