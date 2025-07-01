6a774e770f8511d1d871a4f85c33264b543a7aed
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-01 20:26:06 +0530

    modification

--- src/api/apiApplications.js
+++ src/api/apiApplications.js
@@ -1,16 +1,31 @@
-import supabaseClient from "@/utils/supabase";
+import supabaseClient, { supabaseUrl } from "@/utils/supabase";
 
 export async function applyToJob(token, _, jobData) {
   try {
     const supabase = await supabaseClient(token);
-    
+
     const random = Math.floor(Math.random() * 90000);
     const fileName = `resume-${random}-${jobData.candidate_id}`;
-    await supabase.storage.from("resumes").upload(fileName, jobData.resume);
+    const { error: storageError } = await supabase.storage
+      .from("resumes")
+      .upload(fileName, jobData.resume);
+    if (storageError) {
+      console.log("Error uploading resumes", error);
+      return [];
+    }
+    const resume = `${supabaseUrl}/storage/v1/object/public/resumes/${fileName}`;
 
-    const { data, error } = await supabase.from("companies").select("*");
+    const { data, error } = await supabase
+      .from("applications")
+      .insert([
+        {
+          ...jobData,
+          resume,
+        },
+      ])
+      .select();
     if (error) {
-      console.log("Error in fetching the companies", error);
+      console.log("Error in submitting Application", error);
       return [];
     }
     return data;
