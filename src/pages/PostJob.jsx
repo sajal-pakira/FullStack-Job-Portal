a2f89436b1baca38d71a870456a5512566f44b3b
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-05 12:03:36 +0530

    schema added

--- src/pages/PostJob.jsx
+++ src/pages/PostJob.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { zodResolver } from "@hookform/resolvers/zod";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const schema = z.object({
+  title: z.string.min(1, { message: "Title is required" }),
+  description: z.string.min(1, { message: "Description is required" }),
+  location: z.string.min(1, { message: "Location is required" }),
+  company_id: z.string.min(1, { message: "Select or Add a new company" }),
+  requirements: z.string.min(1, { message: "requirements are required" }),
+});
 
 const PostJob = () => {
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      location: "",
+      company_id: "",
+      requirements: "",
+    },
+    resolver: zodResolver(schema),
+  });
   return (
-    <div>PostJob</div>
-  )
-}
+    <div>
+      <h1>Post a Job</h1>
+    </div>
+  );
+};
 
-export default PostJob
+export default PostJob;
