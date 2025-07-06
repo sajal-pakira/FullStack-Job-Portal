0884b9ead567bd1cf9e9cafa1817b4c7c3a4548f
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-06 21:18:12 +0530

    schema added

--- src/components/addCompanyDrawer.jsx
+++ src/components/addCompanyDrawer.jsx
@@ -1,4 +1,8 @@
-import React from "react";
+import { z } from "zod";
+
+const schema = z.object({
+  name: z.string().min(1, { message: "Company name is required" }),
+});
 
 const AddCompanyDrawer = (fetchCompanies) => {
   return <div>AddCompanyDrawer</div>;
