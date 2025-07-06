e12c67c552c23de19c9d8e2ef6d73b8e55893e64
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-06 21:11:20 +0530

    modification

--- src/api/apiCompanies.js
+++ src/api/apiCompanies.js
@@ -17,38 +17,33 @@ export async function getCompanies(token) {
 }
 
 export async function addNewCompany(token, _, companyData) {
-  try {
-    const supabase = supabaseClient(token);
+  const supabase = supabaseClient(token);
 
-    const random = Math.floor(Math.random() * 90000);
-    const fileName = `resume-${random}-${companyData.name}`;
-    const { error: storageError } = await supabase.storage
-      .from("company-logo")
-      .upload(fileName, companyData.logo);
+  const random = Math.floor(Math.random() * 90000);
+  const fileName = `resume-${random}-${companyData.name}`;
+  const { error: storageError } = await supabase.storage
+    .from("company-logo")
+    .upload(fileName, companyData.logo);
 
-    if (storageError) {
-      console.log("Error uploading company logo", storageError);
-      return { error: "Resume upload failed" };
-    }
-
-    const logo_url = `${supabaseUrl}/storage/v1/object/public/company-logo/${fileName}`;
-
-    const { data, error } = await supabase
-      .from("companies")
-      .insert([
-        {
-          name: companyData.name,
-          logo_url,
-        },
-      ])
-      .select();
-    if (error) {
-      console.log("Error in submitting the companies", error);
-      return [];
-    }
-    return data;
-  } catch (error) {
-    console.log("ERROR", error);
-    return [];
+  if (storageError) {
+    console.log("Error uploading company logo", storageError);
+    return { error: "Resume upload failed" };
   }
+
+  const logo_url = `${supabaseUrl}/storage/v1/object/public/company-logo/${fileName}`;
+
+  const { data, error } = await supabase
+    .from("companies")
+    .insert([
+      {
+        name: companyData.name,
+        logo_url,
+      },
+    ])
+    .select();
+  if (error) {
+    console.log("Error in submitting the companies", error);
+    return null;
+  }
+  return data;
 }
