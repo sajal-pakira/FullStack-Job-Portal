57aac8e6e209315103fbc87645b52802e4581d2c
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-07 16:19:34 +0530

    BarLoader added

--- src/components/CreatedJobs.jsx
+++ src/components/CreatedJobs.jsx
@@ -18,6 +18,10 @@ const CreatedJobs = () => {
     if (isLoaded) fnCreatedJobs();
   }, [isLoaded]);
 
+  if (loadingCreatedJobs || !isLoaded) {
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+  }
+
   return <div></div>;
 };
 
