cb32b63ca97796ac202dcdaba81fc41ff6336365
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-07 16:07:43 +0530

    modification

--- src/components/CreatedJobs.jsx
+++ src/components/CreatedJobs.jsx
@@ -1,7 +1,18 @@
+import { getMyJobs } from "@/api/apiJobs";
+import useFetch from "@/hooks/useFetch";
+import { useUser } from "@clerk/clerk-react";
+
 const CreatedJobs = () => {
-  return <div>
-    
-  </div>;
+  const { user } = useUser();
+
+  const {
+    fn: fnCreatedJobs,
+    data: createdJobs,
+    loading: loadingCreatedJobs,
+  } = useFetch(getMyJobs, {
+    recruiter_id: user.id,
+  });
+  return <div></div>;
 };
 
 export default CreatedJobs;
