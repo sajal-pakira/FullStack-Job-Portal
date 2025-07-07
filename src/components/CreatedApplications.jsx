cdefaaacf91ec209a2f3f945ca2a10e660848095
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-07 11:46:38 +0530

    CreatedJobs.jsx & CreatedApplications.jsx are added

---
+++ src/components/CreatedApplications.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreatedApplications = () => {
+  return (
+    <div>CreatedApplications</div>
+  )
+}
+
+export default CreatedApplications
--- src/pages/MyJobs.jsx
+++ src/pages/MyJobs.jsx
@@ -1,3 +1,5 @@
+import CreatedApplications from "@/components/CreatedApplications";
+import CreatedJobs from "@/components/CreatedJobs";
 import { useUser } from "@clerk/clerk-react";
 import { BarLoader } from "react-spinners";
 
@@ -6,7 +8,20 @@ const MyJobs = () => {
   if (!isLoaded) {
     return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
   }
-  return <div>MyJobs</div>;
+  return (
+    <div>
+      <h1 className="gradient-title font-extrabold text-5xl sm:text-7xl text-center pb-8">
+        {user?.unsafeMetadata?.role === "candidate"
+          ? "My Applications"
+          : "My Jobs"}
+      </h1>
+      {user?.unsafeMetadata?.role === "candidate" ? (
+        <CreatedApplications />
+      ) : (
+        <CreatedJobs />
+      )}
+    </div>
+  );
 };
 
 export default MyJobs;
