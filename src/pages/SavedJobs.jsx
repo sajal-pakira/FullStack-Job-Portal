aefc674d2e68e3480937ee171b7a8d0eca005d53
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-07 10:40:35 +0530

    Saved Jobs heading is added

--- src/pages/SavedJobs.jsx
+++ src/pages/SavedJobs.jsx
@@ -21,7 +21,13 @@ const SavedJobs = () => {
     return <BarLoader className="mt-4" width={"100%"} color="#36d7b7" />;
   }
 
-  return <div>SavedJobs</div>;
+  return (
+    <div>
+      <h1 className="gradient-title font-extrabold text-6xl sm:text-7xl text-center pb-8">
+        Saved Jobs
+      </h1>
+    </div>
+  );
 };
 
 export default SavedJobs;
