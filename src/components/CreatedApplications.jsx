d340e7f0c8680de52e1a78fab517037b6022374b
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-07 11:59:54 +0530

    BarLoader added

--- src/components/CreatedApplications.jsx
+++ src/components/CreatedApplications.jsx
@@ -1,7 +1,8 @@
 import { getApplications } from "@/api/apiApplications";
 import useFetch from "@/hooks/useFetch";
 import { useUser } from "@clerk/clerk-react";
-import React from "react";
+import React, { useEffect } from "react";
+import { BarLoader } from "react-spinners";
 
 const CreatedApplications = () => {
   const { user } = useUser();
@@ -12,6 +13,14 @@ const CreatedApplications = () => {
   } = useFetch(getApplications, {
     user_id: user.id,
   });
+
+  useEffect(() => {
+    fnApplications();
+  }, []);
+
+  if (loadingApplications) {
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+  }
 
   return <div></div>;
 };
