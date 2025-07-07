bd21fbefab7dcae7b80ab29e74aaca1fef3cd0c3
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-07 10:34:49 +0530

    BarLoader added

--- src/pages/SavedJobs.jsx
+++ src/pages/SavedJobs.jsx
@@ -1,12 +1,20 @@
 import { getSavedJobs } from "@/api/apiJobs";
 import useFetch from "@/hooks/useFetch";
+import { useUser } from "@clerk/clerk-react";
+import { BarLoader } from "react-spinners";
 
 const SavedJobs = () => {
+  const { isLoaded } = useUser();
+
   const {
     fn: fnSavedJobs,
     data: dataSavedJobs,
     loading: loadingSavedJobs,
   } = useFetch(getSavedJobs);
+
+  if (isLoaded) {
+    return <BarLoader className="mt-4" width={"100%"} color="#36d7b7" />;
+  }
 
   return <div>SavedJobs</div>;
 };
