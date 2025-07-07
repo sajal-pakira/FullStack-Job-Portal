169092f16db1cca76dd4f01cf556f13e19eed3df
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-07 13:41:22 +0530

    Reusing one instance (as long as the token is the same).

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -3,13 +3,37 @@ import { createClient } from "@supabase/supabase-js";
 export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-const supabaseClient = (supabaseAccessToken) => {
-  // console.log("🔐 Creating Supabase client with token:", supabaseAccessToken);
-  const supabase = createClient(supabaseUrl, supabaseKey, {
-    global: { headers: { Authorization: `Bearer ${supabaseAccessToken}` } },
-  });
+// const supabaseClient = (supabaseAccessToken) => {
+//   // console.log("🔐 Creating Supabase client with token:", supabaseAccessToken);
+//   const supabase = createClient(supabaseUrl, supabaseKey, {
+//     global: { headers: { Authorization: `Bearer ${supabaseAccessToken}` } },
+//   });
 
-  return supabase;
+//   return supabase;
+// };
+
+let client = null;
+let lastToken = null;
+
+const supabaseClient = (supabaseAccessToken) => {
+  if (!client || lastToken !== supabaseAccessToken) {
+    console.log(
+      "🔐 Creating new Supabase client with token:",
+      supabaseAccessToken
+    );
+
+    client = createClient(supabaseUrl, supabaseKey, {
+      global: {
+        headers: {
+          Authorization: `Bearer ${supabaseAccessToken}`,
+        },
+      },
+    });
+
+    lastToken = supabaseAccessToken;
+  }
+
+  return client;
 };
 
 export default supabaseClient;
