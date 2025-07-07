be506508cfc44024f608b3f95a235840559e5599
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-07 11:40:51 +0530

    BarLoader added

--- src/pages/MyJobs.jsx
+++ src/pages/MyJobs.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { useUser } from "@clerk/clerk-react";
+import { BarLoader } from "react-spinners";
 
 const MyJobs = () => {
-  return (
-    <div>MyJobs</div>
-  )
-}
+  const { user, isLoaded } = useUser();
+  if (!isLoaded) {
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+  }
+  return <div>MyJobs</div>;
+};
 
-export default MyJobs
+export default MyJobs;
