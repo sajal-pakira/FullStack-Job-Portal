829a2bbb7bdddb259afc66473f33e0d3b1e34143
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-07 11:32:09 +0530

    JobCard is added

--- src/pages/SavedJobs.jsx
+++ src/pages/SavedJobs.jsx
@@ -1,4 +1,5 @@
 import { getSavedJobs } from "@/api/apiJobs";
+import JobCard from "@/components/JobCard";
 import useFetch from "@/hooks/useFetch";
 import { useUser } from "@clerk/clerk-react";
 import { useEffect } from "react";
@@ -9,7 +10,7 @@ const SavedJobs = () => {
 
   const {
     fn: fnSavedJobs,
-    data: dataSavedJobs,
+    data: savedJobs,
     loading: loadingSavedJobs,
   } = useFetch(getSavedJobs);
 
@@ -26,6 +27,25 @@ const SavedJobs = () => {
       <h1 className="gradient-title font-extrabold text-6xl sm:text-7xl text-center pb-8">
         Saved Jobs
       </h1>
+
+      {loadingSavedJobs === false && (
+        <div className="mt-8 grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {savedJobs?.length ? (
+            savedJobs.map((saved) => {
+              return (
+                <JobCard
+                  key={saved.id}
+                  job={saved?.job}
+                  savedInit={true}
+                  onJobAction={fnSavedJobs}
+                />
+              );
+            })
+          ) : (
+            <div>No Saved Jobs Found 👀</div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
